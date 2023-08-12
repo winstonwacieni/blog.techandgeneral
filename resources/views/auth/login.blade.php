280fdbf15bbb56fc6eb50b30616aa022647e96a1
obrien-reece <indeche.evans.o@gmail.com>
2023-08-12 16:14:57 +0300

    login page css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,78 +2,106 @@
 
 @push('styles')
 
+<style>
+
+/* Split the screen in half */
+.split {
+  height: 100%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+/* Control the left side */
+.left {
+    left: 0;
+    background-image: url({{ asset('img/purplebg.jpg') }});
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 67%;
+}
+
+
+/* Control the right side */
+.right {
+  right: 0;
+  background-color: #fff;
+  width: 33%;
+}
+
+/* Center right content horizontally and vertically */
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+/*Center left content*/
+.centered-left {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+/* Media query for screens with width less than 1000px and height less than 1000px */
+@media (max-width: 1000px) and (max-height: 1000px) {
+    .left {
+        display: none;
+    }
+
+    .right {
+        width: 100%;
+        left: 0;
+        top: auto;
+        bottom: 0;
+    }
+}
+span, li{
+    color: #fff;
+}
+
+</style>
 
 @endpush()
 
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="split left">
+      <div class="centered-left">
+          <img src="{{ asset('img/logo.png') }}" width="190px" alt="">
+          <br>
+          <span style="font-size: 40pt">Welcome To Tech and General</span>
+          <br>
+          <div>
+              <div style="border-bottom: 2px solid green; display: inline-block;">
+                  <span style="font-size: 37px">Create an account to unlock these benefits</span>
+              </div>
+          </div>
+          <div>
+              <div style="display: inline-block;">
+                  <ul style="text-align: left; padding-right: 160px; list-style-type: disc;font-size: 26px">
+                      <li style="padding-bottom: 10px">Comment on articles and express yourself</li>
+                      <li style="padding-bottom: 10px">Manage your Newsletters</li>
+                      <li style="padding-bottom: 10px">Have a personal profile</li>
+                      <li style="padding-bottom: 10px">Browse without the distraction of popup ads</li>
+                  </ul>
+              </div>
+          </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-</div>
+
+    <div class="split right">
+      <div class="centered">
+        <h2>John Doe</h2>
+        <p>Some text here too.</p>
+      </div>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <met a name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -13,15 +13,12 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-
     @stack('styles')
 
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{--<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -73,7 +70,7 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav>--}}
 
         <main class="py-4">
             @yield('content')
